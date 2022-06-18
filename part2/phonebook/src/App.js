c207242ba00ef086d1c80df12153477d4db7d80d
pulperi-mooc <pulperi+mooc@gmail.com>
2022-06-18 16:21:28 +0300

    part2 sub ready

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
 
@@ -11,11 +13,12 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
   const debugShow = false // Show debug stuff
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -33,21 +36,74 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const toggleNotificationMessage = (text, type) => {
+    setNotificationMessage(
+      { 
+        text: text,
+        messageType: type
+      }
+    )
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 3000)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     if (newName && newNumber) {
-      if ((persons.filter((person) => person.name.toLowerCase() === newName.toLowerCase()).length === 0)) 
-        setPersons(persons.concat({name: newName, number: newNumber}))
-      else
-        alert(`${newName} is already added to phonebook`)
+      
+      const personList = persons.filter((person) => person.name.toLowerCase() === newName.toLowerCase())
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+
+      // Add new Person if name not already in phonebook
+      if ((personList.length === 0)) {
+        personService
+          .create(personObject)
+          .then(response => {
+            setPersons(persons.concat(response.data))
+            toggleNotificationMessage(`Added ${response.data.name}`, 'notification')
+          })
+
+      }
+      // If Person already in phonebook, update Person  
+      else {
+        if (window.confirm(`${personObject.name} is already added to phonebook, replace the old number with a new one?`)) {
+          personObject.id = personList[0].id
+          personService
+          .update(personList[0].id, personObject)
+          .then(response => {
+            setPersons(persons.map(p => p.id !== personObject.id ? p : response.data))
+            toggleNotificationMessage(`Updated ${response.data.name}`, 'notification')
+          })
+        }
+      }
     }
     setNewName('')
     setNewNumber('')
   }
 
+  const removePerson = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(person.id)
+        .then( () => {
+          setPersons(persons.filter(p => p.id !== person.id ))
+          toggleNotificationMessage(`Removed ${person.name}!`, 'warning')
+        })
+        .catch(error => {
+          toggleNotificationMessage(`${person.name} not found on server!`, 'warning')
+          setPersons(persons.filter(p => p.id !== person.id ))
+        })
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
       <Filter value={filter} onChange={handleFilterChange} />
       <h3>add a new</h3>
       <PersonForm onSubmit={addPerson} inputValue1={newName} inputValue2={newNumber} inputChange1={handleNameChange} inputChange2={handleNumberChange} />
@@ -56,7 +112,7 @@ const App = () => {
       <div style={{display: debugShow ? 'block' : 'none'}}>debug: {filter}</div>
 
       <h3>Numbers</h3>
-      <Persons persons={persons} filter={filter}/>
+      <Persons persons={persons} filter={filter} remove={removePerson} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,6 +1,13 @@
-const Persons = ({persons, filter}) => {
+const Persons = ({persons, filter, remove}) => {
     return (
-        <>{persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase())).map((person) => <div key={person.name}>{person.name} {person.number}</div>)}</>
+        <>
+            {persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+                .map((person) => 
+                    <div key={person.name}>
+                        {person.name} {person.number}
+                        <button onClick={() => remove(person) }>Delete</button>
+                    </div>)}
+        </>
     )
 }
 
